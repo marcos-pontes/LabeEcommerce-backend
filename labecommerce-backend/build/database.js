07c29895d25d4b246b11c272790e7a75ef3cf919
Marcos Pontes <socrampontes@gmail.com>
2023-06-16 22:07:32 -0300

    fluxo de dados backend

--- labecommerce-backend/build/database.js
+++ labecommerce-backend/build/database.js
@@ -1,6 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.product = exports.user = void 0;
+exports.searchUserByName = exports.searchProductsByName = exports.getAllProducts = exports.getAllUsers = exports.createProduct = exports.createUser = exports.product = exports.user = void 0;
 exports.user = [
     {
         id: "u001",
@@ -31,3 +31,33 @@ exports.product = [
         imageURL: "https://picsum.photos/seed/Monitor/400",
     },
 ];
+const createUser = (id, name, email, password) => {
+    const createdAt = new Date().toISOString();
+    const newUser = { id, name, email, password, createdAt };
+    exports.user.push(newUser);
+    return "Cadastro realizado com sucesso";
+};
+exports.createUser = createUser;
+const createProduct = (id, name, price, description, imageURL) => {
+    const newProduct = { id, name, price, description, imageURL };
+    exports.product.push(newProduct);
+};
+exports.createProduct = createProduct;
+const getAllUsers = () => {
+    return exports.user;
+};
+exports.getAllUsers = getAllUsers;
+const getAllProducts = () => {
+    return exports.product;
+};
+exports.getAllProducts = getAllProducts;
+const searchProductsByName = (name) => {
+    const searchProduct = name.toLowerCase();
+    return exports.product.filter(product => product.name.toLowerCase().includes(searchProduct));
+};
+exports.searchProductsByName = searchProductsByName;
+const searchUserByName = (name) => {
+    const searchUser = name.toLowerCase();
+    return exports.user.filter(user => user.name.toLowerCase().includes(searchUser));
+};
+exports.searchUserByName = searchUserByName;
--- labecommerce-backend/src/index.ts
+++ labecommerce-backend/src/index.ts
@@ -1,3 +1,245 @@
-import { product, user } from "./database";
-console.log(product);
-console.log(user);
+import { TProduct, TUsers } from "./types";
+import {
+  createProduct,
+
+  products,
+  searchProductsByName,
+  searchUserByName,
+  users,
+} from "./database";
+import express, { Request, Response } from "express";
+import cors from "cors";
+
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+app.listen(3003, () => {
+  console.log("Service listening on gate 3003");
+});
+//Get all users
+app.get("/users", (req: Request, res: Response) => {
+  try {
+    const name = req.query.name as string;
+   const userFilter = users.filter((users) =>
+    users.name.toLowerCase().includes(name.toLowerCase())
+  )
+  if(!userFilter.length){
+    res.statusCode=404;
+    throw new Error("User not found")
+  }
+  if(name){
+    res.status(200).send(userFilter);
+  }else{
+    res.status(200).send(users)
+  }
+  } catch (error) {
+    if (error instanceof Error) {
+      res.send(error.message);
+    }
+    res.status(500).send("Unknown error");
+  }
+});
+
+//Get all products
+app.get("/products", (req: Request, res: Response) => {
+  try {
+    const name = req.query.name as string;
+   const prodFilter = products.filter((products) =>
+    products.name.toLowerCase().includes(name.toLowerCase())
+  )
+  if(prodFilter.length){
+    res.statusCode=404;
+    throw new Error("Product not found")
+  }
+  if(name){
+    res.status(200).send(prodFilter);
+  }else{
+    res.status(200).send(products)
+  }
+  } catch (error) {
+    if (error instanceof Error) {
+      res.send(error.message);
+    }
+    res.status(500).send("Unknown error");
+  }
+});
+
+app.get("/users/search", (req: Request, res: Response) => {
+  const name = req.query.name as string;
+  const resposta = searchUserByName(name);
+  console.log(resposta);
+  res.status(200).send(resposta);
+});
+
+//Create a new user
+app.post("/users/add", (req: Request, res: Response) => {
+  try {
+    const { id, name, email, password } = req.body;
+    const createdAt = new Date().toISOString();
+    const newUser: TUsers = { id, name, email, password, createdAt };
+    const repetId = users.find((user) => user.id === id);
+    const repetEmail = users.find((user) => user.email === email);
+    
+    if (!id || !name || !email || !password) {
+      res.statusCode = 428;
+      throw new Error("Check the filled fields");
+    }
+    if (repetId) {
+      res.statusCode = 412;
+      throw new Error("id already exists, try another id");
+    }
+    if (repetEmail) {
+      res.statusCode = 412;
+      throw new Error("email already exists, try another id");
+    }
+    users.push(newUser);
+    res.status(200).send("Successfully registered user");
+    console.log(users);
+  } catch (error) {
+    if (error instanceof Error) {
+      res.send(error.message);
+    }
+    res.status(500).send("Unknown error");
+  }
+});
+
+//Create a new product
+app.post("/products/add", (req: Request, res: Response) => {
+  try {
+    const { id, name, price, description, imageURL } = req.body;
+    const repetId = products.find((products) => products.id === id);
+    
+    if (!id || !name || !price || !description || !imageURL) {
+      res.statusCode = 428;
+      throw new Error("Check the filled fields");
+    }
+    if (repetId) {
+      res.statusCode = 412;
+      throw new Error("id already exists, try another id");
+    }
+    const newProduct: TProduct = { id, name, price, description, imageURL };
+  products.push(newProduct);
+    res.status(200).send("Product added sucessfully");
+  } catch (error) {
+    if (error instanceof Error) {
+      res.send(error.message);
+    }
+    
+    res.status(500).send("Unknown error");
+  }
+});
+//Delete User
+app.delete("/users/:id", (req: Request, res: Response) => {
+  try {
+    const id = req.params.id;
+    const deleteById = users.findIndex((user) => user.id.toLowerCase() === id.toLowerCase());
+    if (!id) {
+      res.statusCode = 400;
+      throw new Error("id already exists, try another id");
+    }
+    if (deleteById < 0) {
+      res.statusCode = 403;
+      throw new Error("product does not exist or has already been deleted");
+    }
+    users.splice(deleteById, 1);
+    res.status(200).send("Successfully deleted user");
+  } catch (error) {
+    if (error instanceof Error) {
+      res.send(error.message);
+    }
+    res.status(500).send("Unknown error");
+  }
+});
+//Delete product
+app.delete("/products/:id", (req: Request, res: Response) => {
+  try {
+    const id = req.params.id;
+    const deleteById = products.findIndex(
+      (product) => product.id.toLowerCase() === id.toLowerCase()
+    );
+    if (!id) {
+      res.statusCode = 400;
+      throw new Error("id already exists, try another id");
+    }
+    if (deleteById < 0) {
+      res.statusCode = 404;
+      throw new Error("product does not exist or has already been deleted");
+    }
+    products.splice(deleteById, 1);
+    res.status(200).send("Successfully deleted product");
+  } catch (error) {
+    if (error instanceof Error) {
+      res.send(error.message);
+    }
+    res.status(500).send("Unknown error");
+  }
+});
+//Edit user
+app.put("/users/:id", (req: Request, res: Response) => {
+  try {
+    const id = req.params.id;
+  const { id: newId, name, email, password } = req.body;
+  const findUser = users.find((user) => user.id.toLowerCase() === id.toLowerCase());
+
+  if(newId && newId[0] !=='u'){
+    res.statusCode = 400;
+    throw new Error('The ID field must start with the letter "u".')
+  }
+
+  if (findUser) {
+    findUser.id = newId || findUser.id;
+    findUser.name = name || findUser.name;
+    findUser.email = email || findUser.email;
+    findUser.password = password || findUser.password;
+    
+  } else {
+    res.statusCode = 404;
+    throw new Error('User not edited, check information')
+  }
+  res.status(200).send("Successfully updated user");
+  } catch (error) {
+    if (error instanceof Error) {
+      res.send(error.message);
+    }
+    res.status(500).send("Unknown error");
+  }
+  
+});
+//Edit product
+app.put("/products/:id", (req: Request, res: Response) => {
+  try {
+    const id = req.params.id;
+  const { id: newId, name, price, description, imageURL } = req.body;
+  const findproduct = products.find(
+    (product) => product.id.toLowerCase() === id.toLowerCase()
+  );
+  const findIndex = products.findIndex((product) => product.id.toLowerCase() === id.toLowerCase())
+
+    if (findIndex <0){
+
+    }
+
+  if(newId && newId[0] !=='p'){
+    res.statusCode = 400;
+    throw new Error('The ID field must start with the letter "p".')
+  }
+
+  if (findproduct) {
+    findproduct.id = newId || findproduct.id;
+    findproduct.name = name || findproduct.name;
+    findproduct.price = price || findproduct.price;
+    findproduct.description = description || findproduct.description;
+    findproduct.imageURL = imageURL || findproduct.imageURL;
+    res.status(200).send("Successfully updated product");
+    
+  }
+    
+  } catch (error) {
+    if (error instanceof Error) {
+      res.send(error.message);
+    }
+    res.status(500).send("Unknown error");
+  }
+});
